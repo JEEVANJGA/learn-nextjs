c31b9e368b9fe94b5e4174c19802320346b1e28e
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2025-02-04 22:57:42 +0530

    Add Hello client component and update Home page layout | Server Side Component vs client side component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import Hello from "@/app/components/hello.client";
+
 export default function Home() {
+  console.log("Which component am I?");
+
   return (
-    <div className="my-10 px-24 bg-white">
-      <div className="py-5 flex flex-row justify-center font-bold text-5xl shadow-xl">
-        Welcome to Next.js.
+    <main>
+      <div className="my-10 px-24 bg-white">
+        <div className="py-5 flex flex-row justify-center font-bold text-5xl shadow-xl">
+          Welcome to Next.js.
+        </div>
       </div>
-    </div>
+      <Hello />
+    </main>
   );
 }
