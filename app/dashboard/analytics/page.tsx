91d2165b37fce48ea5fadc5231d2bf3eef998db5
Jeevan George Antony <antony.jeevangeorge@gmail.com>
2025-02-13 11:26:21 +0530

    Nested Routes | Add dashboard pages for Analytics, Main Dashboard, and Users

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,13 @@
+const page = () => {
+  return (
+    <main>
+      <div className="my-10 px-24 bg-white">
+        <div className="py-5 flex flex-row justify-center font-bold text-5xl shadow-xl">
+          Analytics
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
